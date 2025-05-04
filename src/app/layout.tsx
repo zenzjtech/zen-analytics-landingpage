44b5baee0c6a8a8b64637aa3c607c68e8bc49f8d
zenji <canhtoannguyen60@gmail.com>
2025-05-04 07:51:27 +0700

    feature: set up customize theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,19 @@
 import * as React from 'react';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import theme from '@/theme';
 import InitColorSchemeScript from '@mui/material/InitColorSchemeScript';
-import ModeSwitch from '@/components/ModeSwitch';
+import ThemeRegistry from '@/components/ThemeRegistry';
+
 
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        <InitColorSchemeScript attribute="class" />
-        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
-          <ThemeProvider theme={theme}>
-            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-            <CssBaseline />
-            <ModeSwitch />
+        {/* <InitColorSchemeScript attribute="class" /> */}
+        <ThemeRegistry>
+          <AppRouterCacheProvider options={{ enableCssLayer: true }}>
             {props.children}
-          </ThemeProvider>
-        </AppRouterCacheProvider>
+          </AppRouterCacheProvider>
+        </ThemeRegistry>      
       </body>
     </html>
   );
