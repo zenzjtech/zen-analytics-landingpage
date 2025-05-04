4ed3b3eb55b97054fadbecd72c8e024f1b099882
zenji <canhtoannguyen60@gmail.com>
2025-05-04 09:41:32 +0700

    feat: add font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,11 @@ import ThemeRegistry from '@/components/ThemeRegistry';
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet" />
+      </head>
       <body>
         {/* <InitColorSchemeScript attribute="class" /> */}
         <ThemeRegistry>
